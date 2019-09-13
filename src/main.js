18c206721a7ecb1343d142b3f36b6d7da0c31845
Joel Stockamp <stockampj@gmail.com>
2019-09-13 16:56:10 -0700

    add styling to display

--- src/main.js
+++ src/main.js
@@ -2,24 +2,57 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {SearchRequest, ResponseArray, getDoctors, responseArray, logger, fillPage} from './project';
-
-const apiKey = process.env.exports.apiKey;
+import {SearchRequest, ResponseArray, getDoctors, responseArray, logger, timeDelay} from './project';
 
 
 $(document).ready(function(event){
-  let conditionTerm = "query=Headache";
+  const apiKey = process.env.exports.apiKey;
+  let conditionTerm;
   // Portland location=45.505,-122.6750,100
   let location = "&location=45.52345%2C-122.67621%2C100";
   let userLocation = "&user_location=45.52345%2C-122.67621";
 
-  getDoctors(conditionTerm,location,userLocation,apiKey);
+  // gets user's ip address
+  // $.get('http://jsonip.com', function (res) {
+  //     let ip = res.ip;
+  //   });
 
-  $("#logger").click(function(){
-    responseArray.doctorList();
-    console.log(responseArray.doctors)
-  })
+  function fillPage(){
+    responseArray.doctors.forEach(function(doctor){
+      let available;
+      if (doctor.status === true){
+        available = "Accepting new patients!";
+      } else {
+        available = "Not accepting new patients.";
+      }
+      $(".output").append(`
+        <div class ="doc-profile">
+          <a href="${doctor.website}">
+          <h4 class="name">${doctor.firstName} ${doctor.lastName}</h4>
+          </a>
+          <h6>${doctor.specialty}</h6>
+          <h6 class="address">${doctor.address}</h6>
+          <h6 class="phone">${doctor.phone}</h6>
+          <h6 class="status">${available}</h6>
+        </div>
+        `)
+    });
+  }
 
-  fillPage();
+
+  $("#search").click(function(){
+    responseArray.doctors = [];
+    conditionTerm = "query="+$("#conditionSearch").val();
+    getDoctors(conditionTerm,location,userLocation,apiKey);
+    timeDelay(logger, 2000);
+    timeDelay(fillPage, 2500);
+
+
+  });
+
+
+
+
+
 
 });
--- src/project.js
+++ src/project.js
@@ -1,7 +1,8 @@
 export class Doctor {
-  constructor(firstName,lastName,address,phone,website,status) {
+  constructor(firstName,lastName,specialty,address,phone,website,status) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.specialty = specialty;
     this.address = address;
     this.phone = phone;
     this.website = website;
@@ -16,6 +17,12 @@ export class SearchRequest {
   }
 }
 
+export class Conditions {
+  constructor(){
+    this.conditionsList = [];
+  }
+}
+
 export class ResponseArray {
   constructor (){
     this.object;
@@ -23,16 +30,21 @@ export class ResponseArray {
   }
   doctorList(){
     for (let i=0; i< this.object.length; i++) {
-      let doc = this.object[i];
-      const firstName = doc.profile.first_name;
-      const lastName = doc.profile.first_name;
-      const address = doc.practices[0].visit_address;
-      const addressString = address.street +", "+ address.city+ ", " + address.state +", " + address.zip;
-      const phone = doc.practices[0].phones[0].number;
-      const website = "www.google.com"
-      const status = doc.practices[0].accepts_new_patients;
-      const doctor = new Doctor(firstName,lastName,addressString,phone,website,status);
-      this.doctors.push(doctor);
+      const doc = this.object[i];
+      const title = doc.profile.title;
+      if (title === "MD") {
+        console.log(doc)
+        const firstName = doc.profile.first_name;
+        const lastName = doc.profile.last_name;
+        const specialty = doc.specialties[0].name;
+        const address = doc.practices[0].visit_address;
+        const addressString = address.street +", "+ address.city+ ", " + address.state +", " + address.zip;
+        const phone = doc.practices[0].phones[0].number;
+        const website = doc.practices[0].website;
+        const status = doc.practices[0].accepts_new_patients;
+        const doctor = new Doctor(firstName,lastName,specialty,addressString,phone,website,status);
+        this.doctors.push(doctor);
+      } 
     }
   }
 }
@@ -42,7 +54,7 @@ export const responseArray = new ResponseArray();
 export function getDoctors(conditionTerm, location, userLocation, apiKey) {
   let promise = new Promise(function(resolve,reject){
     let request = new XMLHttpRequest();
-    const url = `https:\//api.betterdoctor.com/2016-03-01/doctors?${conditionTerm}${location}${userLocation}&skip=0&limit=10&user_key=${apiKey}`;
+    const url = `https:\//api.betterdoctor.com/2016-03-01/doctors?${conditionTerm}${location}${userLocation}&skip=0&limit=30&user_key=${apiKey}`;
     request.onload = function (){
       if(this.status === 200){
         resolve(request.response);
@@ -66,6 +78,6 @@ export function logger(){
   console.log(responseArray.doctors)
 }
 
-export function fillPage() {
-  let myVar = setTimeout(logger, 3000);
+export function timeDelay(action, delay) {
+  let myVar = setTimeout(action, delay);
 }
