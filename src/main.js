5ffa9bac00b41ee284e3f5cdfd4b9d9cb6599db8
Joel Stockamp <stockampj@gmail.com>
2019-09-13 13:22:08 -0700

    add timer to fill page with object return after set time

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {SearchRequest, ResponseArray} from './project';
+import {SearchRequest, ResponseArray, getDoctors, responseArray, logger, fillPage} from './project';
 
 const apiKey = process.env.exports.apiKey;
-const responseArray = new ResponseArray();
 
 
 $(document).ready(function(event){
@@ -14,34 +13,13 @@ $(document).ready(function(event){
   let location = "&location=45.52345%2C-122.67621%2C100";
   let userLocation = "&user_location=45.52345%2C-122.67621";
 
-
-  function getDoctors(conditionTerm, location) {
-    let promise = new Promise(function(resolve,reject){
-      let request = new XMLHttpRequest();
-      const url = `https:\//api.betterdoctor.com/2016-03-01/doctors?${conditionTerm}${location}${userLocation}&skip=0&limit=10&user_key=${apiKey}`;
-      request.onload = function (){
-        if(this.status === 200){
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET",url,true);
-      request.send();
-    });
-
-    let apiResponse;
-    promise.then(function(response){
-      let apiResponse = JSON.parse(response);
-      responseArray.object = apiResponse;
-
-
-    })
-  }
-  getDoctors(conditionTerm,location);
+  getDoctors(conditionTerm,location,userLocation,apiKey);
 
   $("#logger").click(function(){
-    console.log(responseArray.object)
+    responseArray.doctorList();
+    console.log(responseArray.doctors)
   })
+
+  fillPage();
 
 });
--- src/project.js
+++ src/project.js
@@ -1,3 +1,14 @@
+export class Doctor {
+  constructor(firstName,lastName,address,phone,website,status) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phone = phone;
+    this.website = website;
+    this.status = status;
+  }
+}
+
 export class SearchRequest {
   constructor (conditionTerm, location) {
     this.condition = conditionTerm;
@@ -8,5 +19,53 @@ export class SearchRequest {
 export class ResponseArray {
   constructor (){
     this.object;
+    this.doctors = [];
   }
+  doctorList(){
+    for (let i=0; i< this.object.length; i++) {
+      let doc = this.object[i];
+      const firstName = doc.profile.first_name;
+      const lastName = doc.profile.first_name;
+      const address = doc.practices[0].visit_address;
+      const addressString = address.street +", "+ address.city+ ", " + address.state +", " + address.zip;
+      const phone = doc.practices[0].phones[0].number;
+      const website = "www.google.com"
+      const status = doc.practices[0].accepts_new_patients;
+      const doctor = new Doctor(firstName,lastName,addressString,phone,website,status);
+      this.doctors.push(doctor);
+    }
+  }
+}
+
+export const responseArray = new ResponseArray();
+
+export function getDoctors(conditionTerm, location, userLocation, apiKey) {
+  let promise = new Promise(function(resolve,reject){
+    let request = new XMLHttpRequest();
+    const url = `https:\//api.betterdoctor.com/2016-03-01/doctors?${conditionTerm}${location}${userLocation}&skip=0&limit=10&user_key=${apiKey}`;
+    request.onload = function (){
+      if(this.status === 200){
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET",url,true);
+    request.send();
+  });
+
+  let apiResponse;
+  promise.then(function(response){
+    let apiResponse = JSON.parse(response);
+    responseArray.object = apiResponse.data;
+  })
+}
+
+export function logger(){
+  responseArray.doctorList();
+  console.log(responseArray.doctors)
+}
+
+export function fillPage() {
+  let myVar = setTimeout(logger, 3000);
 }
